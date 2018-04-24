d17c1d67b38d34f2018b7f8ac10f6dd4fc13cb8b
Daniel Pérez Rada <dperezrada@gmail.com>
2018-04-24 13:58:50 +0200

    ADD: json response

--- server.py
+++ server.py
@@ -4,17 +4,24 @@
 import json
 
 from flask import Flask, request
+from flask_cors import CORS
 
 from parser_util import get_terms
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/search-parser", methods=['POST'])
 def search_parser():
     query = request.get_data()
-    return json.dumps(get_terms(query))
+    response = app.response_class(
+        response=json.dumps(get_terms(query)),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(port=5002, host="0.0.0.0", threaded=True)
